82d0581bea4851ad73a734dbbde8a3ec12e97c17
Elayna-hyy <3098045977@qq.com>
2025-09-06 20:49:06 +0800

    写了一个小项目if语句，三只小猪称体重

--- c language learn.c
+++ c language learn.c
@@ -2,6 +2,40 @@
 
 int main()
 {
-    printf("Hello, World!\n");
-    return 0;
+	//三只小猪称体重
+
+	int a, b, c;
+	printf("please input weight of three pigs A:");
+	scanf("%d", &a);
+	printf("please input weight of three pigs B:");
+	scanf("%d", &b);
+	printf("please input weight of three pigs C:");
+	scanf("%d", &c);
+	if (a > b)
+	{
+		if (a > c)
+		{
+			printf("Pig A is the heaviest");
+		}
+		else
+		{
+			printf("Pig C is the heaviest");
+		}
+	}
+	else
+	{
+		if (b > c)
+		{
+			printf("Pig B is the heaviest");
+		}
+		else
+		{
+			printf("Pig C is the heaviest");
+		}
+	}
+	
+
+
+	system("pause");
+	return 0;
 }
