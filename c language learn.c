6d89f649474ae0fac2d0035b88cbb4ad27061812
Elayna-hyy <3098045977@qq.com>
2025-09-07 10:17:06 +0800

    2025/9/7 写了两个小题分别是“数组求最大值”和“数组求和”，其中遇到了一点小问题就是随机数无重复

--- c language learn.c
+++ c language learn.c
@@ -2,30 +2,74 @@
 #include<stdlib.h>
 #include<time.h>
 
-int main()
+int contrast(int arr[], int len, int random)
 {
-	printf("please input a num(1~100)\n");
-	srand(time(NULL));
-	int random = rand() % 100 + 1; 
-	int guess = 0;
-
-	while (1)
+	for (int a = 0; a < len; a++)
 	{
-		scanf("%d", &guess);
-		if (random > guess)
+		if (arr[a]==random)
 		{
-			printf("less\n");
-		}
-		else if (random < guess)
-		{
-			printf("more\n");
-		}
-		else
-		{
-			printf("yes\n");
-			break;
+			return 1;
 		}
 	}
+	return 0;
+}
+
+int main()
+{ 
+	//1.Find the maximum value
+	int arr[] = {33,5,22,44,55};
+	int max = arr[0];
+	int len = sizeof(arr) / sizeof(arr[0]);
+	for (int i = 0; i < len; i++)
+	{
+		if (arr[i] < arr[i + 1])
+		{
+			max = arr[i + 1];
+		}
+	}
+	printf("%d\n", max);
+
+	//2.Traverse the array and sum it up
+	srand(time(NULL));
+	int arr[10] = {0};
+	int len = sizeof(arr) / sizeof(arr[0]);
+
+	for (int a =0; a < len ; )
+	{
+		int random = rand() % 100 + 1;
+		int flag =contrast(arr, len, random);
+		if (!flag)
+		{
+			arr[a] = random;
+			a++;
+		}
+	}
+
+	for (int a = 0; a < len; a++)
+	{
+		printf("%d\t", arr[a]);
+	}
+	printf("\n");
+
+	int sum = 0;
+	for (int a = 0; a < len; a++)
+	{
+		sum = sum + arr[a];
+	}
+	printf("The sum of the random array is :%d\n", sum);
+
+	int ave = sum /len;
+	printf("The average of the random array is :%d\n", ave);
+
+	int less = 0;
+	for (int a = 0; a < len; a++)
+	{
+		if (arr[a] < ave)
+		{
+			less++;
+		}
+	}
+	printf("a total of %d numbers less than the average\n", less);
 	
 
 
