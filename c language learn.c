0cea2e407cd7d1bf1636d476b3fd6eb0d11a69ef
Elayna-hyy <3098045977@qq.com>
2025-09-08 10:23:07 +0800

    2025/9/8，写了个小题目“选择排序”，类似于冒泡排序，冒泡排序是将最大的一直排到最右边，选择排序是将最小的检索一直排到最左边

--- c language learn.c
+++ c language learn.c
@@ -6,34 +6,28 @@
 
 int main()
 { 
-	int arr[8] = { 2,7,1,9,5,6,8,4 };
+	int arr[5] = { 3,5,2,1,4 };
 	int len = sizeof(arr) / sizeof(int);
-	
-	for (int i = 0; i < len - 1; i++)
+
+	for (int i = 0; i < len-1; i++)
 	{
-		//内循环把bubblesort执行了一次
-		//外循环把bubblesort动作重复了len -1次
-		for (int j = 0; j < len - 1-i; j++)
+		for (int j = i+1; j < len; j++)
 		{
-			if (arr[j] > arr[j + 1])
+			if (arr[i] > arr[j])
 			{
-				int temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j]  = temp;
 			}
 		}
 	}
-	
 
-	for (int j = 0; j < len; j++)
+	for (int i = 0; i < len ; i++)
 	{
-		printf("%d  ", arr[j]);
+		printf("%d  ", arr[i]);
 	}
 
 
-
-	
-	
 
 
 	system("pause");
