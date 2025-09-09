6eb6e6562034a1a75b83a3bb9c44ac8d8541bc2f
Elayna-hyy <3098045977@qq.com>
2025-09-09 15:53:52 +0800

    2025/9/8，学习二维数组的遍历，分为一维数组的等长和不等长

--- c language learn.c
+++ c language learn.c
@@ -2,44 +2,52 @@
 #include<stdlib.h>
 #include<time.h>
 
-//将函数结果和状态分开的例子
-/*int remainder(int num1, int num2, int res)
-{
-	if (num2 == 0)//因此要写一个判断，但是必须要返回一个值，由于是求余数，所以返回多少都不合适
-	{
-		return ?:
-	}
-	res = num1 % num2;//假如num2=0时就报错了
-	return res;
-}*/
-int remainder(int num1, int num2, int* res)
-{
-	if (num2 == 0)
-	{
-		return 1;
-	}
-	*res = num1 % num2;//此时写成用返回值表示状态，0正常 1不正常，就可以了
-	return 0;
-}
 
 int main()
 {
-	int a = 10;
-	int b = 4;
-	int res = 0;
-	int flag = remainder(a, b, &res);
-	//对状态进行判断
-	if (flag == 0)
+	/*int arr[3][5] =
 	{
-		printf("the remainder is %d\n", res);
-	}
+	{ 1,2,3,11,22 },
+	{ 4,5,6 ,44,55},
+	{ 7,8,9 ,77,88}
+	};
 
+	for (int i = 0; i < 3; i++)
+	{
+		//外循环遍历二维数组中的索引
+		for (int j = 0; j < 5; j++)
+		{
+			//内循环遍历每一个一维数组
+			printf("%d  ", arr[i][j]);
+		}
+		printf("\n");
+	}*/
+
+
+
+	/*int arr1[] = { 1,2,3 };
+	int arr2[] = { 1,2,3,4,5 };
+	int arr3[] = { 1,2,3,4,5,6,7,8,9 };
+
+	int len1 = sizeof(arr1) / sizeof(int);
+	int len2 = sizeof(arr2) / sizeof(int);
+	int len3 = sizeof(arr3) / sizeof(int);
+
+	int lenarr[3] = { len1,len2,len3 };
+
+	int* arr[3] = { arr1,arr2,arr3 };//传递的是一维数组的地址
+	//此时使用arr1的数组名进行计算的时候，会退化到第一个元素的指针，不再表示整体
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < lenarr[i]; j++)
+		{
+			printf("%d  ", arr[i][j]);
+		}
+		printf("\n");
+	}*/
 
 
 	system("pause");
 	return 0;
 }
-
-
-
-
