d4c111a03712242082b92e3c2fc2a4c1930e9b37
Elayna-hyy <3098045977@qq.com>
2025-09-09 15:55:39 +0800

    2025/9/9，学习二维数组的指针遍历

--- c language learn.c
+++ c language learn.c
@@ -5,47 +5,23 @@
 
 int main()
 {
-	/*int arr[3][5] =
+	int arr[2][5] =
 	{
-	{ 1,2,3,11,22 },
-	{ 4,5,6 ,44,55},
-	{ 7,8,9 ,77,88}
+		{1,2,3,4,5},
+		{11,22,33,44,55}
 	};
 
-	for (int i = 0; i < 3; i++)
+	int(*p)[5] = arr;
+
+	for (int i = 0; i < 2; i++)
 	{
-		//外循环遍历二维数组中的索引
 		for (int j = 0; j < 5; j++)
 		{
-			//内循环遍历每一个一维数组
-			printf("%d  ", arr[i][j]);
+			printf("%d  ", *(*p + j));
 		}
 		printf("\n");
-	}*/
-
-
-
-	/*int arr1[] = { 1,2,3 };
-	int arr2[] = { 1,2,3,4,5 };
-	int arr3[] = { 1,2,3,4,5,6,7,8,9 };
-
-	int len1 = sizeof(arr1) / sizeof(int);
-	int len2 = sizeof(arr2) / sizeof(int);
-	int len3 = sizeof(arr3) / sizeof(int);
-
-	int lenarr[3] = { len1,len2,len3 };
-
-	int* arr[3] = { arr1,arr2,arr3 };//传递的是一维数组的地址
-	//此时使用arr1的数组名进行计算的时候，会退化到第一个元素的指针，不再表示整体
-
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < lenarr[i]; j++)
-		{
-			printf("%d  ", arr[i][j]);
-		}
-		printf("\n");
-	}*/
+		p++;
+	}
 
 
 	system("pause");
