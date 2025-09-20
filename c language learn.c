cee689f732aaddb67e806a991f592167643d2ee4
Elayna-hyy <3098045977@qq.com>
2025-09-20 20:28:52 +0800

    2025/9/20，学习了遍历字符数组的两种方法

--- c language learn.c
+++ c language learn.c
@@ -6,38 +6,35 @@
 
 int main()
 {
-	char* ri_account = "zhangsan";
-	char* ri_code = "123456";
-
-	
-
-	for (int i = 1; i <= 3; i++)
+	char strarr[5][100] =
 	{
-		printf("please input your account name\n");
-		char account[100];
-		scanf("%s", account);
+		"zhangsan",
+		"lisi",
+		"wangwu",
+		"zhaoliu",
+		"qianqi"
+	};
 
-		printf("please input your password\n");
-		char code[100];
-		scanf("%s", code);
-		if (!strcmp(ri_account, account) && !strcmp(ri_code, code))//该函数返回值为0，0为假，故取反
-		{
-			printf("YES\n");
-			break;
-		}
-		else
-		{
-			if (i == 3)
-			{
-				printf("用户%s被锁定，请联系0858\n", ri_account);
-			}
-			else
-			{
-				printf("NO");
-				printf("你还有%d次机会\n", 3 - i);
-			}
-		}
+	for (int i = 0; i < 5; i++)
+	{
+		char* str = strarr[i];
+		//不能用 char str[100]=strarr[i]的方式去遍历
+		printf("%s\n", str);
+	}
 
+	char* strarr2[5] =
+	{
+		"zhangsan",
+		"lisi",
+		"wangwu",
+		"zhaoliu",
+		"qianqi"
+	};
+
+	for (int i = 0; i < 5; i++)
+	{
+		char* str = strarr2[i];
+		printf("%s\n", str);
 	}
 
 
