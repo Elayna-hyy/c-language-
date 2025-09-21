d5f34ad78f7d191ce9105651b5015d33f400c85b
Elayna-hyy <3098045977@qq.com>
2025-09-21 15:55:17 +0800

    2025/9/21，写了一个小问题投票选举，综合运用了数组遍历，随机数，最大值检索的知识

--- c language learn.c
+++ c language learn.c
@@ -1,35 +1,61 @@
 #include <stdio.h>
-#include<stdlib.h>
-#include<time.h>
 #include<string.h>
+#include<time.h>
+#include<stdlib.h>
 
-typedef struct student
+
+struct spot
 {
 	char name[100];
-	int age;
-}S;
+	int count;
+};
 
-void alter(S* p)
-{
-	printf("修改前数据为：%s,%d\n", (*p).name, (*p).age);
-	printf("请输入名字\n");
-	scanf("%s", (*p).name);
-	printf("请输入年龄\n");
-	scanf("%d", &((*p).age));
-	printf("修改后数据为：%s,%d\n", (*p).name, (*p).age);
-}
+
+
 
 int main()
 {
-	//细节：函数中写结构体类型的变量，相当于是定义了一个新变量
-	//		  ：在函数中仅仅修改了st的值，对于main函数中的s1并没有修改
+	struct spot arr[4] = { {"A",0} ,{ "B",0 }, { "C",0 }, { "D",0 } };
+	srand(time(NULL));
+	for (int i = 0; i < 80; i++)
+	{
+		//choose既表示投票也表示索引
+		int choose = rand() % 4;
+		arr[choose].count++;
+	}
+
 	
-	S stu;
-	strcpy(stu.name, "aaa");
-	stu.age = 0;
-	printf("初始数据:%s,%d\n", stu.name, stu.age);
-	alter(&stu);
-	printf("结尾数据:%s,%d\n", stu.name, stu.age);
+	for (int i = 0; i < 4; i++)
+	{
+		struct spot temp = arr[i];
+		printf("%s %d\n", temp.name,temp.count);
+	}
+
+	int max = arr[0].count;
+	for (int i = 0; i < 4; i++)
+	{
+		if (arr[i].count < arr[i+1].count)
+		{
+			max = arr[i + 1].count;
+		}
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		struct spot temp = arr[i];
+		if (temp.count==max)
+		{
+			printf("投票数最多的景点为%s，共计：%d张票\n", arr[i].name, arr[i].count);
+			break;
+		}
+	}
+
+
+
+	
+	
+	
+	
 
 	system("pause");
 	return 0;
