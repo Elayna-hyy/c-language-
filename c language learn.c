ad827f551cbaba835aba467f2db2f8be8c2d70c2
Elayna-hyy <3098045977@qq.com>
2025-09-21 10:36:25 +0800

    2025/9/21，学习了如何通过指针传递结构体，重点在于解引用

--- c language learn.c
+++ c language learn.c
@@ -3,35 +3,35 @@
 #include<time.h>
 #include<string.h>
 
-typedef struct ulrtraman
+typedef struct student
 {
 	char name[100];
-	int attack;
-	int defence;
-	int blood;
-}M;
+	int age;
+}S;
+
+void alter(S* p)
+{
+	printf("修改前数据为：%s,%d\n", (*p).name, (*p).age);
+	printf("请输入名字\n");
+	scanf("%s", (*p).name);
+	printf("请输入年龄\n");
+	scanf("%d", &((*p).age));
+	printf("修改后数据为：%s,%d\n", (*p).name, (*p).age);
+}
+
 int main()
 {
-	//结构体 起别名
-	M taro = { "泰罗",100,90,500 };
-	M rem = { "雷欧",90,70,100 };
-	M eddie = { "艾迪",80,60,400 };
-
-	M arr[3] = { taro,rem,eddie };
-	for (int i = 0; i < 3; i++)
-	{
-		M temp =  arr[i];
-		printf("名字：%s，攻击力：%d，防御力：%d，血量：%d\n", temp.name, temp.attack, temp.defence, temp.blood);
-	}
-
+	//细节：函数中写结构体类型的变量，相当于是定义了一个新变量
+	//		  ：在函数中仅仅修改了st的值，对于main函数中的s1并没有修改
 	
-	
-
-
-
-
-
+	S stu;
+	strcpy(stu.name, "aaa");
+	stu.age = 0;
+	printf("初始数据:%s,%d\n", stu.name, stu.age);
+	alter(&stu);
+	printf("结尾数据:%s,%d\n", stu.name, stu.age);
 
 	system("pause");
 	return 0;
 }
+
