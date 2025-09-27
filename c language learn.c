5a1dc79aa0f79083a134fa1f5a744b1eb8767500
Elayna-hyy <3098045977@qq.com>
2025-09-27 09:56:50 +0800

    2025/9/27，学习了记事本文件中的读取和输出，用到了五个函数，fopen,fgetc,fgets,fread,fclose

--- c language learn.c
+++ c language learn.c
@@ -1,38 +1,42 @@
 #include <stdio.h>
-#include<string.h>
-#include<stdlib.h>
-
-struct linkman
-{
-	char name[20];
-	int age;
-};
-
-struct roster
-{
-	struct linkman arr;
-	int size;
-};
-
-void add(struct roster* p)
-{
-	printf("请输入姓名：");
-	scanf("%s", p->arr.name);
-	printf("姓名：%s\n", p->arr.name);
-}
-
+#include <string.h>
+#include <stdlib.h>
+   
 int main()
 {
-	//结构体嵌套 指针键盘赋值
-	struct roster* p=malloc(sizeof(struct roster));
-	add(p);
 	
+	FILE* file = fopen("D:\\OneDrive\\桌面\\test1.txt","r");
+	//读不到返回-1
+	//char arr[1024];
+	//char* str;
+	//while ((str = fgets(arr, 1024, file)) != NULL)
+	//{
+	//	printf("%s", str);
+	//}
+	// //读取数据 以换行符为准
+	//fclose(file);
+
+	//abc占用一个字节，中文win64，默认两个字节，读取时尽可能装满字节
+	//文件100字节
+	//数组长度30字节
+	//前三次读取30个字节，会把数组装满，函数返回30
+	//第四次读取剩余30字节，把数据放回数组中，返回10
+	//第五次没有数据可被读取，函数返回0
+
+	//文件中数据：abc你好你好
+	//第一次读取：abc+半个你
+	//第二次读取：半个你+好+半个你
+	//第三次读取：半个你+好
+	char arr[4];
+	int n;
+	while ((n=fread(arr, 1, 1024, file)) != 0)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			printf("%c", arr[i]);
+		}
+	}
 
 
-	system("pause");
 	return 0;
 }
-
-
-
-
